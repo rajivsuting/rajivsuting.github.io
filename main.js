5b6ed0ee46777c1d4906c249a66150aa3cfec5a1
rajivsuting <rajivsuting16@gmail.com>
2022-09-15 13:16:15 -0700

    try

--- main.js
+++ main.js
@@ -6,5 +6,21 @@ $(document).ready(function(){
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle')
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+    })
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        }),
+        500,
+        'linear'
     })
 })
